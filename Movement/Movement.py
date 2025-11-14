65b9b11cb6199604dd56786989bf4a8986f69be4
Kapithijn <153297911+Kapithijn@users.noreply.github.com>
2025-11-14 11:28:13 +0100

    movement class

---
+++ Movement/Movement.py
@@ -0,0 +1,11 @@
+"""
+This code consist of the code needed for the movement of the robot.
+Classes:
+- Movement: Contains methods to control the robot's movement.
+"""
+
+class Movement:
+    def __init__(self, movement_parameters= None):
+        self.movement_parameters = movement_parameters
+
+
